dab2e6857a7d636f8722884c1d7e416b95228a10
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-24 17:24:34 -0300

    chore: setup Classification model

--- classifier/models.py
+++ classifier/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Classification(models.Model):
+    image = models.ImageField(upload_to='results/')
+    result = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
