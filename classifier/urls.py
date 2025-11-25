66ed1ecd626973e3391c2ef2756e781f1196b35e
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-24 23:40:27 -0300

    feat: submit image for classification

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home)
-]
+    path('', home, name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
