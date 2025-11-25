9962f113cac4ed9b5d2962fc5c86c6b727f7139f
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-25 10:09:20 -0300

    feat(classifier): add views to query and return Classification data

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, get_results, get_filtered_result
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
+    path('results/', get_results, name='results'),
+    path('results/<int:pk>/', get_filtered_result, name='filtered_result'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- classifier/views.py
+++ classifier/views.py
@@ -12,3 +12,15 @@ def home(request):
             context = {'classification': classification}
             return render(request, 'classifier/index.html', context)
     return render(request, 'classifier/index.html')
+
+def get_results(request):
+    images = Classification.objects.all()
+    context = {'results': images}
+    return render(request, 'classifier/results.html', context)
+
+def get_filtered_result(request, pk):
+    image = Classification.objects.filter(id=pk)
+    if image.exists():
+        context = {'result': image.first()}
+        return render(request, 'classifier/filtered.html', context)
+    
