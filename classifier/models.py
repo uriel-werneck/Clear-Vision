eec11abc49cd5dbe50f2e578d65c1dcd121bc462
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-27 22:48:46 -0300

    feat(classifier): store original and result image in Classification model

--- classifier/models.py
+++ classifier/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Classification(models.Model):
-    image = models.ImageField(upload_to='results/')
-    result = models.CharField(max_length=100)
+    original_image = models.ImageField(upload_to='originals/', default=None)
+    result_image = models.ImageField(upload_to='results/', default=None)
+    description = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
