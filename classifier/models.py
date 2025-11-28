9d6fa54c21125c13a827cd2a6f989fe5b353a19a
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-28 12:25:38 -0300

    chore(classifier): add new fields to Classification model to store result image and crack confidence

--- classifier/models.py
+++ classifier/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Classification(models.Model):
     original_image = models.ImageField(upload_to='originals/', default=None)
     result_image = models.ImageField(upload_to='results/', default=None)
-    description = models.CharField(max_length=100)
+    cracked_confidence = models.FloatField(default=0.0)
+    uncracked_confidence = models.FloatField(default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
