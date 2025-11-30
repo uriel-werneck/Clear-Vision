8e1b5c6085edfa248153428d668bf46f86babe33
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-30 13:33:13 -0300

    feat(classifier): remove locally stored image on delete

--- classifier/models.py
+++ classifier/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Classification(models.Model):
@@ -7,3 +8,10 @@ class Classification(models.Model):
     cracked_confidence = models.FloatField(default=0.0)
     uncracked_confidence = models.FloatField(default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        if self.original_image and os.path.isfile(self.original_image.path):
+            os.remove(self.original_image.path)
+        if self.result_image and os.path.isfile(self.result_image.path):
+            os.remove(self.result_image.path)
+        super().delete(*args, **kwargs)
